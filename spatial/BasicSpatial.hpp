9e30c17b9cdcdd05b097d133b7d2c9f1b37a93ee
ZzAZz4 <esteban.villacorta@utec.edu.pe>
2021-09-10 08:25:07 -0500

    a

--- spatial/BasicSpatial.hpp
+++ spatial/BasicSpatial.hpp
@@ -24,4 +24,24 @@ class BasicSpatial : public SpatialBase<Point> {
 }  // namespace spatial
 }  // namespace utec
 
-#include "BasicSpatial.inl"
+#include <algorithm>
+
+namespace utec {
+namespace spatial {
+
+template <typename Point>
+void BasicSpatial<Point>::insert(const Point& new_point) {
+  this->values.push_back(new_point);
+}
+
+template <typename Point>
+Point BasicSpatial<Point>::nearest_neighbor(const Point& reference) {
+  const auto closest_to_ref = [&reference](const Point& a, const Point& b) {
+    return a.distance(reference) < b.distance(reference);
+  };
+  return *std::min_element(values.cbegin(), values.cend(), closest_to_ref);
+}
+
+}  // namespace spatial
+}  // namespace utec
+
